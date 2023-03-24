a08c672608c3771ab412d9411bc1c35dbb2c68cd
celinenolf-5 <luna0093@hotmail.com>
2023-03-24 20:54:20 +0100

    first commit

---
+++ 40bierges/src/views/login/login.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+import { Redirect } from 'react-router-dom'
+
+
+// core components
+import tools from "../../toolBox"
+import axios from "axios";
+
+
+class Login extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      redirected: false,
+      redirectedAdmin: false,
+      mail: "",
+      password: "",
+      url: "http://localhost:3001",
+      counter:0
+    };
+    this.handleConnect = this.handleConnect.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+  };
+
+  componentDidMount() {
+    if (tools.checkIfConnected()) {
+      this.setState({ redirected: true })
+    }
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  handleConnect() {
+    this.setState({counter:this.state.counter+1})
+    if(this.state.counter==20){
+      alert("Coucou")
+    }
+    if (this.state.mail === '' || this.state.password === '') {
+      alert('Please fill in all fields of the form')
+      return;
+    }
+    if (!/\S+@\S+\.\S+/.test(this.state.mail)) {
+      alert('The mail does not correspond to the right format')
+      return;
+    }
+    axios.post(this.state.url + '/connection', {
+      mail: this.state.mail,
+      password: this.state.password
+    }).then(response => {
+      if (response.status === 200) {
+        let d = new Date();
+        d.setTime(d.getTime() + (3 * 24 * 60 * 60 * 1000));
+        let expires = "expires=" + d.toUTCString();
+        document.cookie = "Token=" + response.data.token + ";" + expires + ";path=/"
+        if (response.data.role === "user") {
+          this.setState({ redirected: true })
+        } else if (response.data.role === "admin") {
+          this.setState({ redirectedAdmin: true })
+        }
+      } else {
+        alert("error " + response.status)
+      }
+    }).catch(error => {
+      console.log(error)
+    });
+  }
+
+  render() {
+    if (this.state.redirected) return (<Redirect to="/index" />)
+    if (this.state.redirectedAdmin) return (<Redirect to="/admin" />)
+    return (
+      <>
+        <div className="wrapper fadeInDown">
+          <div id="formContent">
+            <h2 className="active"> Sign In </h2>
+            <h2 className="inactive underlineHover">Sign Up </h2>
+            
+          <input type="text" name="mail" id="login" value={this.state.mail} onChange={this.handleChange} className="fadeIn second" placeholder="mail"></input>
+          <input type="password" name="password" className="fadeIn third" value={this.state.password} onChange={this.handleChange} placeholder="password"></input>
+          <button onClick={this.handleConnect}>Se connecter</button>
+          {this.state.counter > 3?this.state.counter:""}
+          
+        </div>
+        </div>
+      </>
+    )
+  }
+}
+
+export default Login;
